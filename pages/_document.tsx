988359e1ad5321546d1878b1ca221d2f8a1e82b1
ikram-maulana <ikram075@ummi.ac.id>
2022-10-01 10:53:19 +0700

    Feat(Lang): Add Lang in HTML Tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html className="dark scroll-smooth">
+    <Html className="dark scroll-smooth" lang="en">
       <Head></Head>
       <body>
         <Main />
