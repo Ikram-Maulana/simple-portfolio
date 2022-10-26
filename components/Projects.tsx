8f28e8a3dcd26b5756741838ebdd9fff5bf13c6e
Ikram Maulana <ikram075@ummi.ac.id>
2022-10-26 14:04:35 +0700

    Feat(UI): Change Color and Style

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -29,10 +29,10 @@ const Projects = ({
         className="object-cover rounded-md image-projects"
         unoptimized={true}
       />
-      <h2 className="mt-3 mb-2.5 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+      <h2 className="mt-3 mb-2.5 text-2xl font-bold tracking-tight text-gray-900 dark:text-[#CCD6F6]">
         {name}
       </h2>
-      <p className="font-normal text-gray-700 dark:text-gray-400">
+      <p className="font-normal text-gray-700 dark:text-[#8892B0]">
         {description.length > 83
           ? description.slice(0, 83) + "..."
           : description}
@@ -41,7 +41,7 @@ const Projects = ({
       <div className="button-projects">
         <Link href={githubLink}>
           <a
-            className="inline-flex py-2.5 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 justify-center items-center"
+            className="inline-flex py-2.5 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-md hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 justify-center items-center"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -51,7 +51,7 @@ const Projects = ({
         </Link>
         <Link href={demoLink}>
           <a target="_blank" rel="noopener noreferrer">
-            <button className="relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800 w-full">
+            <button className="relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-medium text-gray-900 rounded-md group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800 w-full">
               <span className="relative px-5 py-2 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 w-full">
                 Live Demo
               </span>
