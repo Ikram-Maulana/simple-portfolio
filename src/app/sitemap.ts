3ec363dfcc2983b1647f25a2898148500e260cde
Ikram Maulana <ikram075@ummi.ac.id>
2023-04-20 10:43:52 +0700

    Feat(Lib): Add SEO Purpose

---
+++ src/app/sitemap.ts
@@ -0,0 +1,14 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://ikram-maulana.tech",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://ikram-maulana.tech/projects",
+      lastModified: new Date(),
+    },
+  ];
+}
