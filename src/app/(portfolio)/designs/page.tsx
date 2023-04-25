7ab8a194839d55f69b93263501f2ba6d77963224
Ikram Maulana <ikram075@ummi.ac.id>
2023-04-26 06:22:19 +0700

    Feat(UI): Update Designs Page
    Update:
    Change size of not found card into cols-span-2

--- src/app/(portfolio)/designs/page.tsx
+++ src/app/(portfolio)/designs/page.tsx
@@ -43,7 +43,7 @@ export default async function Designs() {
                   ))
                 ) : (
                   <div
-                    className="w-full p-5 rounded-lg justify-items-center lg:p-6"
+                    className="w-full p-5 rounded-lg justify-items-center lg:p-6 col-span-2"
                     style={{
                       backgroundColor: Tokens.colors["fire-opal"]["base"],
                     }}
