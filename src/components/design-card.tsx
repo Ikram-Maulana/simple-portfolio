0ace0e96a33f9fb48e111997b6aaa02614ed0f99
Ikram Maulana <ikram075@ummi.ac.id>
2023-04-25 13:47:47 +0700

    Feat(UI): Update Design Card Component
    Update:
    Make image priority
    Delete width auto because give margin

--- src/components/design-card.tsx
+++ src/components/design-card.tsx
@@ -7,15 +7,15 @@ export default function DesignCard({ item }: DesignCardProps) {
     <div className="w-full">
       <Link href={item.dribbble_url} target="_blank" rel="noopener noreferrer">
         <Image
-          className="rounded-lg brightness-100 hover:brightness-75 transition-all"
+          className="mr-0 transition-all rounded-lg brightness-100 hover:brightness-75"
           src={item.image_url}
           width={800}
           height={600}
           alt={item.name}
           style={{
             height: "auto",
-            width: "auto",
           }}
+          priority
         />
       </Link>
     </div>
