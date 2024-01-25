300df42bb4cd223fcdb5df83325b4a8ba08c809a
Ikram Maulana <ikram075@ummi.ac.id>
2024-01-25 10:09:42 +0700

    chore(url): change base URL

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = process.env.VERCEL_URL || "http://localhost:3000";
+  const baseUrl = "https://ikrammaulana.my.id";
 
   return {
     rules: {
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = process.env.VERCEL_URL || "http://localhost:3000";
+  const baseUrl = "https://ikrammaulana.my.id";
 
   return [
     {
